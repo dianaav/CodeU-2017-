6fc580b38a10b29e84d083d1e022b8b8d78699a6
dianaav <diana_avram_s@yahoo.com>
2017-05-18 19:46:41 +0100

    Updated Exercise1.java

--- Assignment1/Exercise1/Exercise1.java
+++ Assignment1/Exercise1/Exercise1.java
@@ -1,1 +1,11 @@
-
+public class Exercise1{
+	
+	public static void main(String []args){
+		Permutation perm1= new Permutation (“Silent”,”Listen”);
+		Permutation perm2= new Permutation (“Triangle”,”Integral”);
+		Permutation perm3= new Permutation (“Apple”,”Pabble”);
+		System.out.println(“Silent, Listen => ” + perm1.compareStrings());
+		System.out.println(“Triangle, Integral => ” + perm2.compareStrings());
+		System.out.println(“Apple, Pabble => ” + perm3.compareStrings());
+	}
+}
