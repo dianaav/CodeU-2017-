e2e1f437e19c24a5f181305b7ba667c9d30c1a8a
dianaav <diana_avram_s@yahoo.com>
2017-05-18 23:24:25 +0100

    Added main

--- Assignment1/exercise2/Exercise2.java
+++ Assignment1/exercise2/Exercise2.java
@@ -1,1 +1,12 @@
+package exercise2;
 
+public class Exercise2 {
+  public static void main(String[] args){
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    list.add(1, new Integer(2));
+    list.add(2, new Integer(7));
+    list.add(3, new Integer(4));
+    list.add(4, new Integer(5));
+    System.out.println(list.getKth(3));
+  }
+}
